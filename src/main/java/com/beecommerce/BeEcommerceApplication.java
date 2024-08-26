e0862c3fcd0a2abe46ba5b64cbe17bd88cf65702
Anh Thu <thuleanh543@gmail.com>
2024-08-27 00:48:04 +0700

    Initial commit

---
+++ src/main/java/com/beecommerce/BeEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.beecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beecommerce/BeEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
