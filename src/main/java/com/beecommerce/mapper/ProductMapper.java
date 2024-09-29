f0113d008823a8d75cd409976b57c137c0632c0f
Anh Thu <thuleanh543@gmail.com>
2024-09-29 22:49:09 +0700

    Feat: update set sold = 0L

--- src/main/java/com/beecommerce/mapper/ProductMapper.java
+++ src/main/java/com/beecommerce/mapper/ProductMapper.java
@@ -60,8 +60,7 @@ public abstract class ProductMapper {
 
     @Mapping(target = "images", expression = "java(variantRequest.getImages() == null || variantRequest.getImages().isEmpty() ? null : uploadImagesToS3(variantRequest.getImages()))")
     @Mapping(target = "attributes",  expression = "java(mapAttributesProductVariant(variantRequest.getAttributes()))")
-    @Mapping(target = "sold", ignore = true)
-
+    @Mapping(target = "sold", ignore = true, expression = "java(0L)")
     public abstract ProductVariant toProductVariant(ProductVariantRequest variantRequest);
 
     @Mapping(target = "attributes", expression = "java(convertAttributesProductVariantToDisplayNames(productVariant.getAttributes()))")
